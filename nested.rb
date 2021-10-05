e22ef662b68e941cc2c50907d22c6481a6825d10
LatellaL <83371530+LatellaL@users.noreply.github.com>
2021-10-05 14:15:55 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,8 @@ def hopper
           :languages => ["C"]
         }
      }
-
+#Added code
+programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     #Added code
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+    #Added code
+    programmer_hash[:dennis_ritchie][:languages].first
 end
 
 def adding_matz
@@ -79,7 +85,12 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+#Added code
+programmer_hash[:yukihiro_matsumoto] = {
+  :known_for => "ruby", 
+  :languages => ["LISP, C"]
+}
+programmer_hash
     
 end
 
@@ -100,8 +111,10 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
+     #alans_new_info = "GUI"
+     #Added code
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     programmer_hash
      
 end
 
@@ -122,4 +135,7 @@ def adding_to_dennis
         }
      }
 
+  #Added code
+  programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly" ]   
+  programmer_hash
 end
